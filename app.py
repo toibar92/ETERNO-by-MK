62921125ff8c9aed20630b729b409cda5fbcf41b
toibar92 <ti@thewheelcorp.com>
2026-01-21 15:42:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,9 @@ def get_db_connection():
 
 def init_db():
     conn = get_db_connection()
-    cur = conn.cursor()
+    cur = conn.cursor():
+    cur.execute('DROP TABLE IF EXISTS pedidos')
+    cur.execute('DROP TABLE IF EXISTS usuarios')
     
     # Tabla de usuarios
     cur.execute('''
